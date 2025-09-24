172748e07afc4cad102fa5eff61a63772dae5abf
Novashinskiy.Y.M <emailno@bk.ru>
2025-09-24 12:50:38 +0300

    nix: init flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cmake
+          gdb
+          clang
+          clang-tools
+        ];
+      };
+    };
+}
